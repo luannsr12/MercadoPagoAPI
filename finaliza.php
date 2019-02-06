fbc6d3d5cca5929aa6e7a472736aa736f1c6968c
Luan Alves <32730441+luannsr12@users.noreply.github.com>
2019-02-06 15:47:20 -0200

    Update finaliza.php

--- finaliza.php
+++ finaliza.php
@@ -27,7 +27,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+	   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">    
 
     <title>Finalizando - Controle Xbox 360 Wireless Original</title>
   </head>
